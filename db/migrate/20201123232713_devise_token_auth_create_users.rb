eabea3e7a1467a3341b20658388e9b32d211ad84
Marcel Cleisson Pereira <mcleisson@gmail.com>
2020-11-23 20:45:23 -0300

    inclusão de campos na tabela user

--- db/migrate/20201123232713_devise_token_auth_create_users.rb
+++ db/migrate/20201123232713_devise_token_auth_create_users.rb
@@ -29,10 +29,16 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       # t.datetime :locked_at
 
       ## User Info
-      t.string :name
-      t.string :nickname
-      t.string :image
+      t.string :first_name #alterado de name para first_name
+      t.string :last_name #incluso campo last_name
       t.string :email
+      t.string :role #incluso campo regra para utilização no puntid
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
+      
 
       ## Tokens
       t.json :tokens
