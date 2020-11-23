c7300230a3839565e7d8967460551e7d26ae50ee
Marcel Cleisson Pereira <mcleisson@gmail.com>
2020-11-23 20:59:26 -0300

    atualização da Model User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
-
 class User < ActiveRecord::Base
+  extend Devise::Models #added this line to extend devise model
+  
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable and :omniauthabledevise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
